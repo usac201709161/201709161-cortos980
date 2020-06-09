ee458b4fc4d70ddad36868851308c97a3ddfb662
4lexmendoz4 <3009941330101@ingenieria.usac.edu.gt>
2020-06-09 12:42:52 -0600

    final

--- corto1.py
+++ corto1.py
@@ -1,12 +1,17 @@
-def par (num):  #de el valor
-    if num%2 ==0:
-        num = int(num/2)
-    else:
-        num= int((num*3)+1)
-    return num
 
-conteo =0
-while contero != 1:
-    for i in range (2, 161):
-    print(par(i))
-        
+lista=[]
+carnet=161
+
+for i in range (2,carnet):
+    while i !=1:
+
+        if i%2 ==0:
+            lista.append(i)
+            i=int(i/2)
+        else:
+            lista.append(i)
+            i=int (i*3+1)
+print(lista)
+archivo=open('collatz.txt','a')
+archivo.write('lista')
+archivo.close()
